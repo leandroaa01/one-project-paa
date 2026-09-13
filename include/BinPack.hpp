6bec7258764ff34ab6a730aacf5f7c6022e2f53e
Leandro Andrade <leandro.andrade.401@ufrn.edu.br>
2026-09-13 19:46:55 -0300

    Feat(BinPack): Implement Bin and BinPack classes with item addition logic

--- include/BinPack.hpp
+++ include/BinPack.hpp
@@ -10,3 +10,48 @@
  *
  */
 #pragma once
+#include <vector>
+
+struct Bin {
+    int capacity;
+    std::vector<int> list;
+    bool isFull() const {
+        return capacity <= 0;
+    }
+};
+
+class BinPack {
+    private:
+    std::vector<Bin> bins;
+    int binCapacity;
+public:
+    BinPack(int binCapacity) : binCapacity(binCapacity) {
+        bins.push_back(Bin{binCapacity, {}});
+    }
+
+    void addItem(int itemSize) {
+        for (auto& bin : bins) {
+            //se a capacidade do bin for maior ou igual ao tamanho do item, adiciona o item ao bin
+            if (bin.capacity >= itemSize) {
+                bin.list.push_back(itemSize);
+                bin.capacity -= itemSize;
+                return;
+            }
+        }
+        // se o bin não tiver capacidade suficiente, cria um novo bin e adiciona o item a ele
+        bins.push_back(Bin{binCapacity - itemSize, {itemSize}});
+    }
+
+    const std::vector<Bin>& getBins() const {
+        return bins;
+    }
+
+    std::size_t size() const {
+        return bins.size();
+    }
+
+    int getBinCapacity() const {
+        return binCapacity;
+    }
+};
+
